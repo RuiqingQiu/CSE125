7bfd8700d74bd4e3e51ab34201f4af221ad52b09
xiazhvera <zhxia@ACS-CSEB220-02.ad.ucsd.edu>
2015-04-06 20:31:28 -0700

    ADD GAMESTATE AND SERVERCORE

---
+++ GameServer/server/ConsoleApplication1/ServerCore.cpp
@@ -0,0 +1,46 @@
+#include "ServerCore.h"
+
+
+ServerCore::ServerCore()
+{
+	gameState = new GameState();
+	gameLogic = new GameLogic();
+}
+
+
+ServerCore::~ServerCore()
+{
+	delete gameLogic;
+	delete gameState;
+}
+
+
+
+void ServerCore::serverLoop()
+{
+	int timeFrame = 1000;
+
+	while (true)
+	{
+		//unsigned t0 = clock();
+		unsigned int action;
+
+		switch (gameState->getState())
+		{
+		case INIT_STATE0:
+			action = gameLogic.waitToConnect();
+		default:
+			break;
+		}
+
+		gameState->nextState();
+
+
+		//cout << "Sleep the frame." << endl;
+		//unsigned elapsed = clock() - t0;
+		////TimeFrame::frameSleep(timeFrame - elapsed);
+		//Sleep(timeFrame - elapsed);
+		////cout << elapsed << endl;
+		//cout << "Frame has completed sleeping." << endl;
+	}
+}
--- GameServer/server/ConsoleApplication1/main.cpp
+++ GameServer/server/ConsoleApplication1/main.cpp
@@ -3,62 +3,23 @@
 //#include "TimeFrame.h"
 // may need #include "stdafx.h" in visual studio
 #include "stdafx.h"
-#include "ServerGame.h"
 // used for multi-threading
 #include <process.h>
 #include <ctime>
 #include "GameObj.h"
+#include "serverCore.h"
+#include "definition.h"
 
 void serverLoop(void *);
 void clientLoop(void);
 
-ServerGame * server;
-unsigned int gameState;
+ServerCore * serverCore;
+
 int main()
 {
 
-	gameState = 0;
-	// initialize the server, comment if running client
-	server = new ServerGame();
-	
-	serverLoop((void*)12);
+	serverCore = new ServerCore();
+	serverCore->serverLoop();
 
-	//Uncomment this part for running client
-	// initialize the client 
-	//client = new ClientGame();
-
-	//clientLoop();
 }
 
-void serverLoop(void * arg)
-{
-	int timeFrame = 1000;
-	
-	while (true)
-	{
-		unsigned t0 = clock();
-		switch (gameState){
-		case 0: {
-					gameState = server->waitForConnections();
-					break;
-		}
-		case 1:
-			{
-				   //cout << "state 1 " << endl;
-				   server->update();
-				   
-				   //server->sendActionPackets();
-				   break;
-			}
-		}
-		//server->update();
-		
-
-		cout << "Sleep the frame." << endl;
-		unsigned elapsed = clock() - t0;
-		//TimeFrame::frameSleep(timeFrame - elapsed);
-		Sleep(timeFrame - elapsed);
-		//cout << elapsed << endl;
-		cout << "Frame has completed sleeping." << endl;
-	}
-}
