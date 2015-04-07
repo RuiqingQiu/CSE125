07cb0268953c3714f471c0c8893a1f7c66b5bd15
Monica <mcl018@ucsd.edu>
2015-04-06 20:39:40 -0700

    clean up window

--- GUI/window.cpp
+++ GUI/window.cpp
@@ -52,86 +52,46 @@
 #include <QMessageBox>
 
 Window::Window(MainWindow *mw)
-    : mainWindow(mw)
-{
+    : mainWindow(mw) {
 
     menu = new mainMenu();
     buildV = new buildView();
 
-    glWidget = new GLWidget;
-
     QDesktopWidget dw;
     setFixedSize(dw.width(), dw.height());
-
-    xSlider = createSlider();
-    ySlider = createSlider();
-    zSlider = createSlider();
-
-    connect(xSlider, SIGNAL(valueChanged(int)), glWidget, SLOT(setXRotation(int)));
-    connect(glWidget, SIGNAL(xRotationChanged(int)), xSlider, SLOT(setValue(int)));
-    connect(ySlider, SIGNAL(valueChanged(int)), glWidget, SLOT(setYRotation(int)));
-    connect(glWidget, SIGNAL(yRotationChanged(int)), ySlider, SLOT(setValue(int)));
-    connect(zSlider, SIGNAL(valueChanged(int)), glWidget, SLOT(setZRotation(int)));
-    connect(glWidget, SIGNAL(zRotationChanged(int)), zSlider, SLOT(setValue(int)));
-
     mainLayout = new QVBoxLayout;
-    QHBoxLayout *container = new QHBoxLayout;
-    container->addWidget(glWidget);
-    container->addWidget(xSlider);
-    container->addWidget(ySlider);
-    container->addWidget(zSlider);
 
-    sampleCode = new QWidget;
-    sampleCode->setLayout(container);
-    //mainLayout->addWidget(sampleCode);
     connect(menu->playButton, SIGNAL(clicked()), this, SLOT(playButtonPressed()));
     mainLayout->addWidget(menu);
-    dockBtn = new QPushButton(tr("Undock"), this);
-    connect(dockBtn, SIGNAL(clicked()), this, SLOT(dockUndock()));
+
+    connect(buildV->dockBtn, SIGNAL(clicked()), this, SLOT(dockUndock()));
     //mainLayout->addWidget(dockBtn);
 
     setLayout(mainLayout);
 
-    xSlider->setValue(15 * 16);
-    ySlider->setValue(345 * 16);
-    zSlider->setValue(0 * 16);
-
     setWindowTitle(tr("Hello GL"));
 }
 
-QSlider *Window::createSlider()
-{
-    QSlider *slider = new QSlider(Qt::Vertical);
-    slider->setRange(0, 360 * 16);
-    slider->setSingleStep(16);
-    slider->setPageStep(15 * 16);
-    slider->setTickInterval(15 * 16);
-    slider->setTickPosition(QSlider::TicksRight);
-    return slider;
-}
-
-void Window::keyPressEvent(QKeyEvent *e)
-{
+void Window::keyPressEvent(QKeyEvent *e) {
     if (e->key() == Qt::Key_Escape)
         close();
     else
         QWidget::keyPressEvent(e);
 }
 
-void Window::dockUndock()
-{
+void Window::dockUndock() {
     if (parent()) {
         setParent(0);
         setAttribute(Qt::WA_DeleteOnClose);
         move(QApplication::desktop()->width() / 2 - width() / 2,
              QApplication::desktop()->height() / 2 - height() / 2);
-        dockBtn->setText(tr("Dock"));
+        buildV->dockBtn->setText(tr("Dock"));
         show();
     } else {
         if (!mainWindow->centralWidget()) {
             if (mainWindow->isVisible()) {
                 setAttribute(Qt::WA_DeleteOnClose, false);
-                dockBtn->setText(tr("Undock"));
+                buildV->dockBtn->setText(tr("Undock"));
                 mainWindow->setCentralWidget(this);
             } else {
                 QMessageBox::information(0, tr("Cannot dock"), tr("Main window already closed"));
