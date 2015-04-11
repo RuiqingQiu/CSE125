fdab5ea78cb85a87dae0952e9a1b0142b930b096
Mingshan Wang <margartewm3@gmail.com>
2015-04-10 17:59:30 -0700

    add the main menu texture

--- Graphics/CSE125Console/CSE125Console/mainmenu.cpp
+++ Graphics/CSE125Console/CSE125Console/mainmenu.cpp
@@ -1,2 +1,44 @@
+#include "stdafx.h"
 #include "mainmenu.h"
+
+
+mainMenu::mainMenu(){
+
+}
+
+mainMenu::mainMenu(int w, int h){
+	width = w;
+	height = h;
+}
+
+mainMenu::~mainMenu(){
+
+}
+
+void mainMenu::VOnRender(){
+	texture[0] = SOIL_load_OGL_texture
+		(
+
+		"uitexture.jpg"
+		//"/Users/ruiqingqiu/Desktop/Qiu_Code/CSE167/CSE167 Final Project/()airFT.tga"
+		//"/Users/Ennuma/Desktop/CSE167_Final_Project/nightsky_north.bmp"
+
+		,
+		SOIL_LOAD_AUTO,
+		SOIL_CREATE_NEW_ID,
+		SOIL_FLAG_INVERT_Y
+		);
+	if (texture[0] == 0)
+	{
+		printf("SOIL loading error: '%s'\n", SOIL_last_result());
+		return;
+	}
+	else{
+		printf("SOIL loading success\n");
+	}
+}
+
+void mainMenu::VOnClientUpdate(GameInfoPacket* info){
+
+}
 
