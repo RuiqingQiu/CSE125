3603c6264dfcfe2a319535c19b4ab69e4485cad8
xiazhvera <zhxia@ACS-CSEB220-26.ad.ucsd.edu>
2015-04-19 17:14:56 -0700

    decode added

--- Graphics/CSE125Console/CSE125Console/ClientGame.cpp
+++ Graphics/CSE125Console/CSE125Console/ClientGame.cpp
@@ -9,6 +9,27 @@ ClientGame::ClientGame(void)
    
 }
 
+//Helper function for split strings
+unsigned int split(const std::string &txt, std::vector<std::string> &strs, char ch)
+{
+	unsigned int pos = txt.find(ch);
+	unsigned int initialPos = 0;
+	strs.clear();
+
+	// Decompose statement
+	while (pos != std::string::npos) {
+		strs.push_back(txt.substr(initialPos, pos - initialPos + 1));
+		initialPos = pos + 1;
+
+		pos = txt.find(ch, initialPos);
+	}
+
+	// Add the last one
+	strs.push_back(txt.substr(initialPos, min(pos, txt.size()) - initialPos + 1));
+
+	return strs.size();
+}
+
 bool ClientGame::connectToServer(char* ipaddress)
 {
 
@@ -64,26 +85,7 @@ bool ClientGame::sendPacket(CPacket packet)
 	printf("send %d\n", packet.packet_type);
 	return NetworkServices::sendMessage(network->ConnectSocket, packet_data, packet_size);
 }
-//Helper function for split strings
-unsigned int split(const std::string &txt, std::vector<std::string> &strs, char ch)
-{
-	unsigned int pos = txt.find(ch);
-	unsigned int initialPos = 0;
-	strs.clear();
 
-	// Decompose statement
-	while (pos != std::string::npos) {
-		strs.push_back(txt.substr(initialPos, pos - initialPos + 1));
-		initialPos = pos + 1;
-
-		pos = txt.find(ch, initialPos);
-	}
-
-	// Add the last one
-	strs.push_back(txt.substr(initialPos, min(pos, txt.size()) - initialPos + 1));
-
-	return strs.size();
-}
 
 GameInfoPacket* ClientGame::update()
 {
@@ -108,53 +110,33 @@ GameInfoPacket* ClientGame::update()
 
 		case GAME_STATE:
 				{
-							 //printf("client received game state packet from server\n");
-							 std::cout << packet.data << std::endl;
+							printf("client received game state packet from server\n");
+							std::cout << packet.data << std::endl;
 						    //cout << "game_state" << endl;
 						    std::string result = std::string(packet.data);
+
 							 if (result == ""){
 								 return nullptr;
 							 }
 							 //Process the result
 							 else{
-								 std::vector<std::string> v;
-								 split(result, v, ' ');
-								 /*
-								 for (int i = 0; i < v.size(); i++){
-									 cout << v[i] << endl;
-								 }*/
-								 //1 + 3 + 16
-								 for (int i = 0; i < v.size()-1; i += 7){
-									 PlayerInfo* p = new PlayerInfo();
-									 p->id = stof(v[i]);
-									 p->x = stof(v[i+1]);
-									 p->y = stof(v[i+2]);
-									 p->z = stof(v[i+3]);
-									 p->rx = stof(v[i + 4])*RAD_TO_DEGREE_MULT;
-									 p->ry = stof(v[i + 5])*RAD_TO_DEGREE_MULT;
-									 p->rz = stof(v[i + 6])*RAD_TO_DEGREE_MULT;
-									 p->processed = false;
-									 //p->print();
-									 /*
-									 for (int j = 0; j < 16; j++){
-										 p->mat[j] = stof(v[i + 4 + j]);
-										// printf("%f, ", p->mat[j]);
-										 if (j % 4 == 0){
-											 //printf("\n");
-										 }
-									 }*/
+								 vector<PlayerInfo*> v = PacketDecoder::decodePacket(result);
+								 for (PlayerInfo* p : v){
 									 g->player_infos.push_back(p);
 								 }
 								 //std::cout << "pushing " << g->player_infos.size() << " on to the list" << std::endl;
 								 g->packet_types = packet.packet_type;
 							 }
-							 //sendActionPackets();
 							 return g;
 							 break;
 				}
+		   /*
+			*	player died, player respawn, explosion, countdown time, score board
+			*   
+			*/
 		case CONFIRM_CONNECTION:
 		{
-								   cout << "fonfirm_packet" << endl;
+			cout << "confirm_packet" << endl;
 			std::string result = std::string(packet.data);
 			std::vector<std::string> v;
 			split(result, v, '\n');
@@ -176,7 +158,7 @@ GameInfoPacket* ClientGame::update()
 					//std::cout << packet.data << std::endl;
 					return nullptr;
 					break;
-		}
+			}
 		}
     }
 	return g;
