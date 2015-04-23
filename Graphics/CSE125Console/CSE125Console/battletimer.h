4399e705561e06aea77576cd7f5235de792645da
Mingshan Wang <margartewm3@gmail.com>
2015-04-22 21:59:55 -0700

    timer works on battleView

--- Graphics/CSE125Console/CSE125Console/battletimer.h
+++ Graphics/CSE125Console/CSE125Console/battletimer.h
@@ -1,13 +1,16 @@
 #include "stdafx.h"
-#include "buildTimer.h"
+#include "guiItem.h"
 #include "numbers.h"
 #include <ctime>
 
 // for display seconds, use buildTimer
 #define MIN 10 // game lasts 10 minutes
+#define MAX_TIME 60
+#define NUM_DIGITS 4
+#define DIGITS 2
 
 // battle view timer extends buildTimer
-class battleTimer : public buildTimer{
+class battleTimer : public guiItem{
 
 public:
 	battleTimer();
@@ -22,12 +25,14 @@ public:
 	void update();
 	void draw();
 	void rePosition(int x, int y, int w, int h);
+	void createNumbers();
 
-	//std::clock_t start;
-	//int timeLeft;
+	std::clock_t start;
+	int minLeft;
+	int secLeft;
 
 	static string textPath;
-
+	numbers * digits[NUM_DIGITS];
 };
 
 
