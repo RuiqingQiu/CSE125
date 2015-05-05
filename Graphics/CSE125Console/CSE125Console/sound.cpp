381779cf3085eb824e1047d118ff03cd0d638e72
Mingshan Wang <margartewm3@gmail.com>
2015-05-04 21:30:19 -0700

    add more sounds

--- Graphics/CSE125Console/CSE125Console/sound.cpp
+++ Graphics/CSE125Console/CSE125Console/sound.cpp
@@ -9,20 +9,38 @@ string path = "sound/";
 
 Sound::Sound(){
 	/* Cannot call play in the constructor !!! */
+
+	// build view selection sound
+	string tmp = path + "select.wav";
+	if (!selectBuffer.loadFromFile(tmp))
+		cout << "ERROR in loading select sound effect " << endl;
+	selectSound.setBuffer(selectBuffer);
+	selectSound.setLoop(true); 
+	// need to play and then pause it
+	selectSound.play();
+	selectSound.pause();
+
 	// game view explosion sound
-	string tmp = path + "explosion.wav";
+	tmp = path + "explosion.wav";
 	if (!explosionBuffer.loadFromFile(tmp))
 		cout << "ERROR in loading explosion sound effect " << endl;
 	explosionSound.setBuffer(explosionBuffer);
-	explosionSound.setPosition(1,0,-5);// create a 3d spatioal sound
-	//explosionSound.play();
+	explosionSound.setPosition(1, 0, -5);// create a 3d spatioal sound
+
+	// game view gun shot sound
+	tmp = path + "gun.wav";
+	if (!gunBuffer.loadFromFile(tmp))
+		cout << "ERROR in loading gun sound effect " << endl;
+	gunSound.setBuffer(gunBuffer);
+	gunSound.play();
+	gunSound.pause();
 
 	// build view selection sound
 	tmp = path + "select.wav";
 	if (!selectBuffer.loadFromFile(tmp))
 		cout << "ERROR in loading explosion sound effect " << endl;
 	selectSound.setBuffer(selectBuffer);
-	selectSound.setLoop(true); 
+	selectSound.setLoop(true);
 	// need to play and then pause it
 	selectSound.play();
 	selectSound.pause();
@@ -46,6 +64,7 @@ void Sound::playMusic(){
 	// for the first time displayCallback calls the playMusic
 	if (music.getStatus() != sf::Sound::Playing)
 		music.play();
+	// without this part still working, so comment out for now
 	/*
 	if (music.getStatus() == sf::Sound::Playing){
 		cout << "Playing music " << endl;
@@ -72,5 +91,16 @@ void Sound::playSelect(){
 		selectSound.play();
 		Sleep(100); // sleep for 0.1 secs and then pause
 		selectSound.pause();
+	}
+}
+
+// This function is used to play gun shot sound
+void Sound::playGun(){
+	cout << "Enter play gun shot effect " << endl;
+	if (gunSound.getStatus() == sf::Sound::Paused){
+		cout << "Play gun " << endl;
+		gunSound.play();
+		Sleep(100); // sleep for 0.1 secs and then pause
+		gunSound.pause();
 	}
 }
--- Graphics/CSE125Console/CSE125Console/sound.h
+++ Graphics/CSE125Console/CSE125Console/sound.h
@@ -15,6 +15,7 @@ public:
 	void playExplosion(); // play the sound 
 	void playMusic();
 	void playSelect(); // play menu selection sound
+	void playGun(); // play gun shot sound
 
 private:	
 	//Init server info here later
@@ -22,10 +23,12 @@ private:
 	sf::SoundBuffer hammerBuffer;
 	sf::SoundBuffer explosionBuffer;
 	sf::SoundBuffer selectBuffer;
+	sf::SoundBuffer gunBuffer;
 
 	sf::Sound explosionSound; 
 	sf::Sound hammerSound;// lightweight obeject that plays loaded audio data from a sound buffer
 	sf::Sound selectSound;
+	sf::Sound gunSound;
 
 	// music will play for the whole game, all other effects are sound
 	sf::Music music;// stream the data on the fly from the source file, play music that lasts several minites
